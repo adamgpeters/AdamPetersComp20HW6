9047063fefcbf2ce8c6d998d5505a535a08abba7
adamgpeters <55671033+adamgpeters@users.noreply.github.com>
2021-03-17 11:21:37 -0400

    Update amicable_numbers.js

--- amicable_numbers.js
+++ amicable_numbers.js
@@ -1,85 +1,88 @@
-var div;
+alert();
 
-function nameLater() {
-    numOne = document.getElementById("numOne").value;
-    numTwo = document.getElementById("numTwo").value;
-    div = document.createElement("div");
-    var numOneP = document.createElement("p");
-    var numTwoP = document.createElement("p");
-    document.body.appendChild(div);                
-    div.appendChild(numOneP);                      
-    div.appendChild(numTwoP);                      
-    numOneP.innerHTML = "NUMBER ONE: " + numOne;
-    numTwoP.innerHTML = "NUMBER TWO: " + numTwo + "</bn>";
 
-    testAmicable(numOne, numTwo);
-    /* testShowArray(); */
-}
+// var div;
 
-function isFactor(num, factor) {
-    if (factor == 0) 
-        return false;
-    return num % factor == 0;
-}
+// function nameLater() {
+//     numOne = document.getElementById("numOne").value;
+//     numTwo = document.getElementById("numTwo").value;
+//     div = document.createElement("div");
+//     var numOneP = document.createElement("p");
+//     var numTwoP = document.createElement("p");
+//     document.body.appendChild(div);                
+//     div.appendChild(numOneP);                      
+//     div.appendChild(numTwoP);                      
+//     numOneP.innerHTML = "NUMBER ONE: " + numOne;
+//     numTwoP.innerHTML = "NUMBER TWO: " + numTwo + "</bn>";
 
-function showArray(arr) {
-    for (let i = 0; i < arr.length; i ++) {
-        let p = document.createElement("p");
-        div.appendChild(p);  
-        p.innerHTML = arr[i];
-    }
-}
+//     testAmicable(numOne, numTwo);
+//     /* testShowArray(); */
+// }
 
-function testIsFactor() {
-    console.log("Expecting true... recieved " + isFactor(100, 10));
-    console.log("Expecting false... recieved " + isFactor(100, 90));
-    console.log("Expecting true... recieved " + isFactor(-90, 9));
-    console.log("Expecting true... recieved " + isFactor(2, -1));
-}
-/* testIsFactor(); */
+// function isFactor(num, factor) {
+//     if (factor == 0) 
+//         return false;
+//     return num % factor == 0;
+// }
 
-function testShowArray() {
-    console.log("Showing [1, 2, 3]...");
-    showArray([1, 2, 3]);
-    console.log("Showing [5, 1, 10]...");
-    showArray([5, 1, 10]);
-    console.log("Showing [-2, \"adam\", true]...");
-    showArray([-2, "adam", true]);
-}
+// function showArray(arr) {
+//     for (let i = 0; i < arr.length; i ++) {
+//         let p = document.createElement("p");
+//         div.appendChild(p);  
+//         p.innerHTML = arr[i];
+//     }
+// }
 
-function addArray(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i ++) 
-        sum += arr[i];
-    return sum;
-}
+// function testIsFactor() {
+//     console.log("Expecting true... recieved " + isFactor(100, 10));
+//     console.log("Expecting false... recieved " + isFactor(100, 90));
+//     console.log("Expecting true... recieved " + isFactor(-90, 9));
+//     console.log("Expecting true... recieved " + isFactor(2, -1));
+// }
+// /* testIsFactor(); */
 
-function testAddArray() {
-    console.log("Expecting 10... recieved " + addArray([2, 3, 5]));
-    console.log("Expecting 0... recieved " + addArray([-10, 10]));
-    console.log("Expecting 0... recieved " + addArray([]));
-}
-// testAddArray();
+// function testShowArray() {
+//     console.log("Showing [1, 2, 3]...");
+//     showArray([1, 2, 3]);
+//     console.log("Showing [5, 1, 10]...");
+//     showArray([5, 1, 10]);
+//     console.log("Showing [-2, \"adam\", true]...");
+//     showArray([-2, "adam", true]);
+// }
 
-function getFactors(num) {
-    let factors = [];
-    for(let i = 1; i < Math.ceil(num / 2) + 1; i ++)
-        if (isFactor(num, i)) factors.push(i);
-    return factors;
-}
+// function addArray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i ++) 
+//         sum += arr[i];
+//     return sum;
+// }
 
-function testGetFactors() {
-    console.log("Expecting [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]... recieved " + getFactors(220));
-    console.log("Expecting [1, 2, 4, 71, 142]... recieved " + getFactors(284));
-    console.log("Expecting [1]... recieved " + getFactors(7));
-}
-/* testGetFactors(); */
+// function testAddArray() {
+//     console.log("Expecting 10... recieved " + addArray([2, 3, 5]));
+//     console.log("Expecting 0... recieved " + addArray([-10, 10]));
+//     console.log("Expecting 0... recieved " + addArray([]));
+// }
+// // testAddArray();
 
-function testAmicable(num1, num2) {
-    let p = document.createElement("p");
-    div.appendChild(p);  
-    if (addArray(getFactors(num1)) == num2 && addArray(getFactors(num2)) == num1)
-        p.innerHTML = num1 + " and " + num2 + " are amicable";
-    else 
-        p.innerHTML = num1 + " and " + num2 + " are not amicable";
-}
+// function getFactors(num) {
+//     let factors = [];
+//     for(let i = 1; i < Math.ceil(num / 2) + 1; i ++)
+//         if (isFactor(num, i)) factors.push(i);
+//     return factors;
+// }
+
+// function testGetFactors() {
+//     console.log("Expecting [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110]... recieved " + getFactors(220));
+//     console.log("Expecting [1, 2, 4, 71, 142]... recieved " + getFactors(284));
+//     console.log("Expecting [1]... recieved " + getFactors(7));
+// }
+// /* testGetFactors(); */
+
+// function testAmicable(num1, num2) {
+//     let p = document.createElement("p");
+//     div.appendChild(p);  
+//     if (addArray(getFactors(num1)) == num2 && addArray(getFactors(num2)) == num1)
+//         p.innerHTML = num1 + " and " + num2 + " are amicable";
+//     else 
+//         p.innerHTML = num1 + " and " + num2 + " are not amicable";
+// }
